3f876af194e1fe73c8c6322a78dc17e60e2892f8
Mark Zeman <mark@speedcurve.com>
2021-10-23 13:26:43 +1300

    Collect Enphase solar data from Envoy production JSON

---
+++ app/Http/Controllers/CollectEnphase.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Carbon\Carbon;
+
+class CollectEnphase extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        // Get Enphase data from the Envoy on the local network
+        $response = Http::get('http://192.168.1.45/production.json');
+        $enphase = $response->json();
+        $time = Carbon::createFromTimestamp($enphase['production'][1]['readingTime'], 'Pacific/Auckland');
+        echo $time->toCookieString() . "<br>";
+        echo "current watts:" . $enphase['production'][1]['wNow'];
+
+        // Format
+        /*
+        Array
+        (
+            [production] => Array
+            (
+                [0] => Array
+                (
+                    [type] => inverters
+                    [activeCount] => 15
+                    [readingTime] => 1634947406
+                    [wNow] => 946
+                    [whLifetime] => 3518
+                )
+                [1] => Array
+                (
+                    [type] => eim
+                    [activeCount] => 1
+                    [measurementType] => production
+                    [readingTime] => 1634947589
+                    [wNow] => 1292.235
+                    [whLifetime] => 3441.176
+                    [varhLeadLifetime] => 0
+                    [varhLagLifetime] => 4882.546
+                    [vahLifetime] => 7613.886
+                    [rmsCurrent] => 5.654
+                    [rmsVoltage] => 236.001
+                    [reactPwr] => 222.661
+                    [apprntPwr] => 1338.249
+                    [pwrFactor] => 0.97
+                    [whToday] => 2844.176
+                    [whLastSevenDays] => 3370.176
+                    [vahToday] => 5323.886
+                    [varhLeadToday] => 0
+                    [varhLagToday] => 3048.546
+                )
+
+        )
+
+    [consumption] => Array
+    (
+        [0] => Array
+        (
+            [type] => eim
+            [activeCount] => 1
+                    [measurementType] => total-consumption
+                    [readingTime] => 1634947589
+                    [wNow] => 5698.263
+                    [whLifetime] => 73308.86
+                    [varhLeadLifetime] => 18153.741
+                    [varhLagLifetime] => 4882.736
+                    [vahLifetime] => 75012.982
+                    [rmsCurrent] => 24.667
+                    [rmsVoltage] => 236.048
+                    [reactPwr] => -1054.396
+                    [apprntPwr] => 5822.639
+                    [pwrFactor] => 0.98
+                    [whToday] => 49255.86
+                    [whLastSevenDays] => 73050.86
+                    [vahToday] => 49884.982
+                    [varhLeadToday] => 11050.741
+                    [varhLagToday] => 3048.736
+                )
+
+            [1] => Array
+    (
+        [type] => eim
+        [activeCount] => 1
+                    [measurementType] => net-consumption
+                    [readingTime] => 1634947589
+                    [wNow] => 4406.029
+                    [whLifetime] => 70146.998
+                    [varhLeadLifetime] => 18153.741
+                    [varhLagLifetime] => 0.19
+                    [vahLifetime] => 75012.982
+                    [rmsCurrent] => 19.013
+                    [rmsVoltage] => 236.095
+                    [reactPwr] => -831.735
+                    [apprntPwr] => 4485.49
+                    [pwrFactor] => 0.98
+                    [whToday] => 0
+                    [whLastSevenDays] => 0
+                    [vahToday] => 0
+                    [varhLeadToday] => 0
+                    [varhLagToday] => 0
+                )
+
+        )
+
+    [storage] => Array
+    (
+        [0] => Array
+        (
+            [type] => acb
+            [activeCount] => 0
+                    [readingTime] => 0
+                    [wNow] => 0
+                    [whNow] => 0
+                    [state] => idle
+                )
+
+        )
+
+    )*/
+
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CollectEnphase;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/enphase', CollectEnphase::class);
