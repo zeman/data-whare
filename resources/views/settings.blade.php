73b1e0cb1c662c09ed75f757f1d770af5b15f348
Mark Zeman <mark@speedcurve.com>
2021-10-31 13:48:35 +1300

    crf needs to be inside form

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -8,22 +8,23 @@
     </head>
     <body>
     @include('nav')
-        @csrf
+
         @if ($data['status'] === "saved")
             <div class="form__saved">Settings saved</div>
         @endif
         <div class="object">
             <form class="form" method="post" action="/settings">
-            <h2>Car</h2>
-            <div class="form__group">
-                <label>Name</label>
-                <input name="car_name" type="text" value="{{ $data['car_name'] }}">
-            </div>
-            <div class="form__group">
-                <label>Teslafi API Token</label>
-                <input name="teslafi_api_token" type="text" value="{{ $data['teslafi_api_token'] }}">
-            </div>
-            <button type="submit">SAVE</button>
+                @csrf
+                <h2>Car</h2>
+                <div class="form__group">
+                    <label>Name</label>
+                    <input name="car_name" type="text" value="{{ $data['car_name'] }}">
+                </div>
+                <div class="form__group">
+                    <label>Teslafi API Token</label>
+                    <input name="teslafi_api_token" type="text" value="{{ $data['teslafi_api_token'] }}">
+                </div>
+                <button type="submit">SAVE</button>
             </form>
         </div>
     </body>
